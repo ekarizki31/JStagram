5c5542bccd13c2d6575acddec759fea30817d91d
Gian Marco <49276912+Metallium96@users.noreply.github.com>
2019-07-23 18:31:13 +0200

    Aggiunta estensione lingua inglese su segui già
    Aggiunta estensione a lingua inglese su funzione che va a verificare se la persona da seguire la si sta seguendo già o gli è stata già inviata una richiesta

--- autoflw.js
+++ autoflw.js
@@ -13,7 +13,16 @@ function autoflw() {
 		
 		while(giaSeguito)
 		{
-			if(!(bottoneFlw.innerText.indexOf("gi") >= 0 || bottoneFlw.innerText.indexOf("eff") >= 0)) {
+			var isFollowed = false
+			isFollowed |= (bottoneFlw.innerText.indexOf("gi") >= 0);	//Già seguito in italiano
+			isFollowed |= (bottoneFlw.innerText.indexOf("eff") >= 0);	//Richiesta effettuata in italiano
+			isFollowed |= (bottoneFlw.innerText.indexOf("ing") >= 0);	//Già seguito in inglese
+			isFollowed |= (bottoneFlw.innerText.indexOf("ted") >= 0);	//Richiesta effettuata in inglese
+			
+			/** Sentitevi liberi di aggiungere condizioni al vostro codice, soprattutto se non siete italiani o inglesi **/
+			/** Feel free to add conditions to your code, especially if you're not english or italian **/
+			
+			if(!(isFollowed)) {
 				//Click sul bottone se non seguo già o non ho effettuato la richiesta
 				bottoneFlw.click();	
 				console.log("Seguo...");
